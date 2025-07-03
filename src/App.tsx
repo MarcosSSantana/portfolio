ea8e858be9ee16d97ddc25f52209fdda8798025c
Marcos Santana <marcosshotokan10@gmail.com>
2025-07-03 17:51:58 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+import Home from "./pages/Home";
+
+function App() {
+  return (<>
+  <h1>App</h1>
+  <Home/>
+  </>);
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-blue-600 dark:text-sky-400 text-xl'>Home</div>
+  )
+}
+
+export default Home
